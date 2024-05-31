d463354b5967b68b4353224d0a7469ee0a023623
Pratyush <pratyushojha059@gmail.com>
2024-05-31 23:59:36 +0545

    started cart page creation

--- frontend/src/context/Context.jsx
+++ frontend/src/context/Context.jsx
@@ -21,10 +21,6 @@ const StoreContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId] : prev[itemId] - 1}));
     }
-
-    useEffect(() => {
-        console.log(cartItems)
-    }, [cartItems])
     
     const contextValue = {
         food_list,
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,26 @@
+import { useContext } from 'react';
 import './Cart.css';
+import { StoreContext } from "../../context/Context";
 
 const Cart = () => {
+
+  const {cartItems, food_list , removeFromCart} = useContext(StoreContext)
+  
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+      </div>
+    </div>
   )
 }
 
