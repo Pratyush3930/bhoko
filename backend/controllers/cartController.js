7ca2866c03e72bb6dd0ad9b7ee961c42ba0efd2e
Pratyush <pratyushojha059@gmail.com>
2024-07-11 22:07:49 +0545

    integrated cart api with the front end and made some minor adjustments

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -23,8 +23,13 @@ const removeFromCart = async (req, res) => {
   try {
     let userData = await userModel.findById(req.body.userId);
     let cartData = await userData.cartData;
-    if (cartData[req.body.itemId] > 0) {
-      cartData[req.body.itemId] -= 1;
+    if (cartData[req.body.itemId] >= 0) {
+      if (cartData[req.body.itemId] === 1) {
+        // Remove the itemId from cartData if it is 0
+        delete cartData[req.body.itemId];
+      } else {
+        cartData[req.body.itemId] -= 1;
+      }
     }
     await userModel.findByIdAndUpdate(req.body.userId, { cartData });
     res.json({ success: true, message: "Removed from cart" });
--- frontend/src/context/Context.jsx
+++ frontend/src/context/Context.jsx
@@ -14,17 +14,23 @@ const StoreContextProvider = (props) => {
 
   const [food_list, setFoodList] = useState([]);
   
-  const addToCart = (itemId) => {
+  const addToCart =async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
       console.log(cartItems)
     }
+    if(token){
+      await axios.post(url + "/api/cart/add", {itemId}, {headers:{token}});
+    }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async(itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if(token){
+      await axios.post(url + "/api/cart/remove", {itemId}, {headers: {token}})
+    }
   };
 
   const getTotalCartAmount = () => {
@@ -49,12 +55,20 @@ const StoreContextProvider = (props) => {
     const res = await axios.get(`${url}/api/food/list`);
     setFoodList(res.data.data);
   }
+
+  // load cartdata from database
+  const loadCartData = async(token) => {
+    const res = await axios.post(url + "/api/cart/get",{}, {headers: {token}});
+    setCartItems(res.data.cartData);
+  }
+
   
   useEffect(() => {
     async function loadData(){
       await fetchFoodList();
       if (localStorage.getItem('token')) {
         setToken(localStorage.getItem('token'));
+        await loadCartData(localStorage.getItem('token'));
       }
     }
     loadData();
